65c98e6ae1e2d3cdc3e4faa61c7e032e0391d193
Sherri Malik <smalik@exchangesolutions.com>
2019-07-19 13:36:04 -0400

    Added python Scripts for testing

---
+++ python/inputs.py
@@ -0,0 +1,5 @@
+print ("\n This is Going to be First Line \n" )
+name = input (" Enter your Name : " )
+col = input (" Enter your favourite Color : " )
+print("\n Thank you \n")
+print(name + " Your Color of the Day is " + col + "\n")
