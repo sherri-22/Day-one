35a668d42a1a35dd5cfb849e92a11ba4071e85a8
Sherri Malik <sherri.malik22@gmail.com>
2019-07-31 15:16:08 -0400

    modified welcome file with some parameter

--- python/welcome.py
+++ python/welcome.py
@@ -5,3 +5,12 @@ to the world of AI""")
 
 print(len(message))
 print(message[0:5])
+print(message.upper())
+print(message.lower())
+print(message.find('World'))
+new_message = message.replace( 'World', 'Universe')
+print(message)
+print(new_message)
+
+greet = 'Hello'
+name = greet
